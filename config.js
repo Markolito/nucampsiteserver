42d47a0f3bb1b8e21d5100bc864cfe8ef564143b
Mark Alindogan <marleoali_777@yahoo.com>
2021-06-01 21:45:39 -0500

    check gitignore

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '532524078129581',
+        clientSecret: '5befff9100d0fd0de683c4c8c15e4abc'
+    }
 }
