1355a8308bfa87e94ba021ccab893171c242d450
Mark Alindogan <marleoali_777@yahoo.com>
2021-06-01 22:33:59 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+const secret = require('./appSecret');
+
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/nucampsite',
     'facebook': {
         clientId: '532524078129581',
-        clientSecret: '5befff9100d0fd0de683c4c8c15e4abc'
+        clientSecret: secret
     }
 }
